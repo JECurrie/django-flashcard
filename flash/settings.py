249fe43bbe568ddafaef0ba68a01bb6c874d77ee
Joan Currie <jecurrie8@gmail.com>
2020-11-26 02:54:40 -0500

    Update settings.py

--- flash/settings.py
+++ flash/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-##DEBUG = False
+DEBUG = False
 
 ALLOWED_HOSTS = ['https://ourflash.herokuapp.com/']
 
